2d897906f134a6b5eeadf3ee2faa01263d6fc8cf
rocksilver <liprock69@gmail.com>
2016-08-18 01:19:20 +0100

    maj

--- j05/ex05/ft_strstr.c
+++ j05/ex05/ft_strstr.c
@@ -3,5 +3,38 @@
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	
+	int i;
+	int j;
+	char *ptr;
+
+	ptr = NULL;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		if (str[i] == to_find[j])
+		{
+		ptr = &str[i];
+			while (str[i] == to_find[j])
+			{
+				if (to_find[j + 1] == '\0')
+					return (ptr);
+				i++;
+				j++;
+			}
+			i--;
+		}
+		i++;
+	}
+	ptr = NULL;
+	return (ptr);
+}
+
+int		main(void)
+{
+	char str[] = "eeeehele  helloee";
+	char to_find[] = "hello";
+
+	ft_putstr(ft_strstr(str, to_find));
+	return (0);
 }
