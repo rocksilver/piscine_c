e508e927720f4fab93b38bd389fbdce8ac2ce804
rocksilver <liprock69@gmail.com>
2016-08-25 01:04:12 +0200

    maj

---
+++ j07/ex05/ft_print_words_tables.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_print_words_tables(char **tab)
+{
+	int i;
+
+	while (tab[i] != 0)
+	{
+		ft_putstr(tab[i]);
+		ft_putchar('\n');
+		i++;
+	}
+}
